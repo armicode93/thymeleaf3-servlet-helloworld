92a1e1c57fcd97868cecafd6b1d1c0e6a1052ac9
armicode93 <naserr93@hotmail.com>
2022-02-01 11:34:30 +0100

    Adding templates

--- src/main/java/web/controller/RegistrationController.java
+++ src/main/java/web/controller/RegistrationController.java
@@ -1,11 +1,9 @@
-package web;
-
+package web.controller;
 
 import config.util.TemplateEngineUtil;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -13,21 +11,18 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 
-@WebServlet("/home")
-public class IndexServlet extends HttpServlet {
-
-
-
+@WebServlet("/registration")
+public class RegistrationController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-        System.out.println("******* SERVLET *******");
+        System.out.println("******* SERVLET 2 *******");
 
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(request.getServletContext());
         WebContext context = new WebContext(request, response, request.getServletContext());
         context.setVariable("recipient", "World");
-        engine.process("home.html", context, response.getWriter());
+        engine.process("registration.html", context, response.getWriter());
     }
 
 }
